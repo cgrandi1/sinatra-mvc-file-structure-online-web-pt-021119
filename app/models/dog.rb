535a9ca4995dd829f8eb79eb1a8bb3938ddea724
Carlton Dwayne <carltondgrandison@gmail.com>
2019-05-22 23:52:05 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,10 +1,10 @@
-class Dog 
+class Dog
 
-  attr_accessor :name, :age, :breed 
+  attr_accessor :name, :age, :breed
   
   def initialize(name, age, breed)
-    @name = name 
-    @age = age 
-    @breed = breed 
-  end 
-end 
+    @name = name
+    @age = age
+    @breed = breed
+  end
+end
